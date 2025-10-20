64fa6ed3c88a89b2116d2740475357014ef9e0e3
apduartte <apduarttel@gmail.com>
2025-10-20 14:15:01 -0300

    script de provisionamento arquivos EFS criptografado e altamente disponível, acessível por instâncias EC2 em sub-redes privadas p/ uso compartilhado pelo Nextcloud

---
+++ nextcloud-aws-terraform/efs.tf
@@ -0,0 +1,20 @@
+
+resource "aws_efs_file_system" "nextcloud" {
+  creation_token   = "nextcloud-efs"
+  performance_mode = "generalPurpose"
+  throughput_mode  = "elastic"
+  encrypted        = true
+  tags = merge(var.tags, { Name = "nextcloud-efs" })
+}
+
+resource "aws_efs_mount_target" "private_1a" {
+  file_system_id  = aws_efs_file_system.nextcloud.id
+  subnet_id       = module.vpc.private_subnets[0]
+  security_groups = [aws_security_group.ec2.id]
+}
+
+resource "aws_efs_mount_target" "private_1b" {
+  file_system_id  = aws_efs_file_system.nextcloud.id
+  subnet_id       = module.vpc.private_subnets[1]
+  security_groups = [aws_security_group.ec2.id]
+}
