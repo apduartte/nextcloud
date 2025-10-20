d93e8f2b1a7f80765114e398eff9f0f9d107936f
apduartte <apduarttel@gmail.com>
2025-10-20 13:56:15 -0300

    criação alb.tf

---
+++ nextcloud-aws-terraform/alb.tf
@@ -0,0 +1,31 @@
+
+module "alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "9.8.0"
+
+  name               = "nextcloud-alb"
+  load_balancer_type = "application"
+  vpc_id             = module.vpc.vpc_id
+  subnets            = module.vpc.public_subnets
+  security_groups    = [aws_security_group.alb.id]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  target_groups = [
+    {
+      name_prefix      = "nxt-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      health_check = { path = "/", port = "80" }
+    }
+  ]
+
+  tags = var.tags
+}
