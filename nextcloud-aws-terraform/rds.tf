19270054bf90c6a5453201810daded06b45d2a99
Ana Paula Duarte <74932085+apduartte@users.noreply.github.com>
2025-11-06 00:41:32 +0000

    Format Terraform files according to terraform fmt

--- nextcloud-aws-terraform/rds.tf
+++ nextcloud-aws-terraform/rds.tf
@@ -8,21 +8,21 @@ resource "aws_db_subnet_group" "nc_db_subnet_group" {
 
 # Instância RDS para o banco PostgreSQL do Nextcloud
 resource "aws_db_instance" "nextcloud_db_instance" {
-  identifier              = "nextcloud-db"
-  engine                  = "postgres"
-  engine_version          = var.db_engine_version
-  instance_class          = var.db_instance_class
-  allocated_storage       = var.db_allocated_gb
-  db_name                 = var.db_name
-  username                = var.db_username
-  password                = var.db_password
-  db_subnet_group_name    = aws_db_subnet_group.nc_db_subnet_group.name
-  vpc_security_group_ids  = [aws_security_group.rds_sg.id]
-  publicly_accessible     = false
-  skip_final_snapshot     = true
-  deletion_protection     = false
-  apply_immediately       = true
-  storage_encrypted       = true
+  identifier             = "nextcloud-db"
+  engine                 = "postgres"
+  engine_version         = var.db_engine_version
+  instance_class         = var.db_instance_class
+  allocated_storage      = var.db_allocated_gb
+  db_name                = var.db_name
+  username               = var.db_username
+  password               = var.db_password
+  db_subnet_group_name   = aws_db_subnet_group.nc_db_subnet_group.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+  publicly_accessible    = false
+  skip_final_snapshot    = true
+  deletion_protection    = false
+  apply_immediately      = true
+  storage_encrypted      = true
 
   tags = merge(var.tags, { Name = "nextcloud-db" })
 }
