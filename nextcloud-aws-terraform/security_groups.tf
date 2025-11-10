8bd10be5ffdc4f05f57e5145fd70a8405cf16dc2
apduartte <apduarttel@gmail.com>
2025-11-10 19:02:28 -0300

    ajuste de configuração terraform_v1

--- nextcloud-aws-terraform/security_groups.tf
+++ nextcloud-aws-terraform/security_groups.tf
@@ -1,87 +1,98 @@
-# Security Group para o Load Balancer (ALB)
+#############################
+# SG do Load Balancer (ALB) #
+#############################
 resource "aws_security_group" "alb_sg" {
   name        = "nextcloud-alb-sg"
-  description = "Permite HTTP/HTTPS"
+  description = "Permite HTTP/HTTPS do público para o ALB"
   vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   tags = merge(var.tags, { Name = "nextcloud-alb-sg" })
 }
 
-# Security Group para instâncias EC2
+# Ingress 80 e 443 do público
+resource "aws_vpc_security_group_ingress_rule" "alb_http_80" {
+  security_group_id = aws_security_group.alb_sg.id
+  ip_protocol       = "tcp"
+  from_port         = 80
+  to_port           = 80
+  cidr_ipv4         = "0.0.0.0/0"
+  description       = "HTTP público"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "alb_https_443" {
+  security_group_id = aws_security_group.alb_sg.id
+  ip_protocol       = "tcp"
+  from_port         = 443
+  to_port           = 443
+  cidr_ipv4         = "0.0.0.0/0"
+  description       = "HTTPS público"
+}
+
+# Egress geral do ALB
+resource "aws_vpc_security_group_egress_rule" "alb_all_out" {
+  security_group_id = aws_security_group.alb_sg.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+  description       = "Saída liberada"
+}
+
+############################
+# SG das instâncias (EC2)  #
+############################
 resource "aws_security_group" "ec2_sg" {
   name        = "nextcloud-ec2-sg"
-  description = "Permite HTTP do ALB e NFS + saída geral"
+  description = "Recebe HTTP do ALB e permite saída geral"
   vpc_id      = module.vpc.vpc_id
-
-  ingress {
-    from_port       = 80
-    to_port         = 80
-    protocol        = "tcp"
-    security_groups = [aws_security_group.alb_sg.id]
-  }
-
-  ingress {
-    from_port   = 2049
-    to_port     = 2049
-    protocol    = "tcp"
-    cidr_blocks = ["10.0.0.0/16"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
 
   tags = merge(var.tags, { Name = "nextcloud-ec2-sg" })
 }
 
-# Security Group para o RDS
-resource "aws_security_group" "rds_sg" {
-  name        = "nc-rds-sg"
-  description = "Permite acesso ao PostgreSQL (porta 5432) a partir da aplicação"
-  vpc_id = module.vpc.vpc_id   
+# Ingress 80 somente a partir do SG do ALB
+resource "aws_vpc_security_group_ingress_rule" "ec2_http_from_alb" {
+  security_group_id              = aws_security_group.ec2_sg.id
+  referenced_security_group_id   = aws_security_group.alb_sg.id
+  ip_protocol                    = "tcp"
+  from_port                      = 80
+  to_port                        = 80
+  description                    = "HTTP vindo do ALB"
 }
 
+# Egress geral para a internet (inclui acesso ao RDS/EFS, updates etc.)
+resource "aws_vpc_security_group_egress_rule" "ec2_all_out" {
+  security_group_id = aws_security_group.ec2_sg.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+  description       = "Saída liberada"
+}
 
-  ingress {
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [aws_security_group.ec2_sg.id]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+########################
+# SG do banco (RDS)    #
+########################
+resource "aws_security_group" "rds_sg" {
+  name        = "nc-rds-sg"
+  description = "Acesso ao PostgreSQL (5432) a partir da aplicação"
+  vpc_id      = module.vpc.vpc_id
 
   tags = merge(var.tags, {
     Name   = "nc-rds-sg"
     Backup = "true"
   })
+}
+
+# Ingress 5432 somente a partir do SG das EC2 (aplicação)
+resource "aws_vpc_security_group_ingress_rule" "rds_pg_from_app" {
+  security_group_id            = aws_security_group.rds_sg.id
+  referenced_security_group_id = aws_security_group.ec2_sg.id
+  ip_protocol                  = "tcp"
+  from_port                    = 5432
+  to_port                      = 5432
+  description                  = "PostgreSQL a partir da app (EC2)"
+}
+
+# Egress do RDS (geralmente liberado; ajuste se tiver política restritiva)
+resource "aws_vpc_security_group_egress_rule" "rds_all_out" {
+  security_group_id = aws_security_group.rds_sg.id
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+  description       = "Saída liberada"
 }
