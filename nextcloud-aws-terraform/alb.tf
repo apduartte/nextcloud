f478d7c1985d70c07b8d06953b60d2abbdd8a64d
apduartte <apduarttel@gmail.com>
2025-11-10 15:19:01 -0300

    ajuste de configuração terraform

--- nextcloud-aws-terraform/alb.tf
+++ nextcloud-aws-terraform/alb.tf
@@ -1,36 +1,47 @@
-module "alb" {
-  source  = "terraform-aws-modules/alb/aws"
-  version = "9.8.0"
 
+resource "aws_lb" "nextcloud_alb" {
   name               = "nextcloud-alb"
+  internal           = false
   load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = module.vpc.public_subnets
 
-  vpc_id         = module.vpc.vpc_id
-  subnets        = module.vpc.public_subnets
-  security_groups = [aws_security_group.alb.id]
+  enable_deletion_protection = false
 
-  listeners = {
-    http = {
-      port     = 80
-      protocol = "HTTP"
-      forward = {
-        target_group_key = "nxt"
-      }
-    }
-  }
+  tags = var.tags
+}
 
-  target_groups = {
-    nxt = {
-      name_prefix = "nxt-"
-      protocol    = "HTTP"
-      port        = 80
-      target_type = "instance"
-      health_check = {
-        path = "/"
-        port = "80"
-      }
-    }
+resource "aws_lb_target_group" "nextcloud_tg" {
+  name_prefix = "nxt-"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "instance"
+  vpc_id      = module.vpc.vpc_id
+
+  health_check {
+    path                = "/"
+    port                = "80"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 5
+    interval            = 30
+    matcher             = "200"
   }
 
   tags = var.tags
+}
+
+resource "aws_lb_listener" "nextcloud_listener" {
+  load_balancer_arn = aws_lb.nextcloud_alb.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nextcloud_tg.arn
+  }
+}
+
+output "alb_dns_name" {
+  value = aws_lb.nextcloud_alb.dns_name
 }
