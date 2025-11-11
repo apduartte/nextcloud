7e975112b0d5d957d5008fe1028437f6c1ec733e
Ana Paula Duarte <74932085+apduartte@users.noreply.github.com>
2025-11-10 23:03:25 -0300

    Update efs.tf

--- nextcloud-aws-terraform/efs.tf
+++ nextcloud-aws-terraform/efs.tf
@@ -1,12 +1,15 @@
 # efs.tf
-resource "aws_efs_file_system" "main" {
-  creation_token = "nextcloud-efs"
-  encrypted      = true
+resource "aws_efs_file_system" "nextcloud" {
+  encrypted        = true
+  performance_mode = "generalPurpose"
+  throughput_mode  = "bursting"
+  tags             = merge(var.tags, { Name = "nextcloud-efs" })
+}
 
-  lifecycle_policy {
-    transition_to_ia = "AFTER_30_DAYS"
-  }
-
-  tags = merge(var.tags, { Name = "nextcloud-efs" })
+resource "aws_efs_mount_target" "nextcloud" {
+  for_each        = toset(module.vpc.private_subnets)
+  file_system_id  = aws_efs_file_system.nextcloud.id
+  subnet_id       = each.value
+  security_groups = [aws_security_group.efs_sg.id]  # ver item 2
 }
  
