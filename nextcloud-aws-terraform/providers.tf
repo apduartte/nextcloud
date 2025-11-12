bd8a3950a467e1ee7e643d8201aef8568d90f2f1
Ana Paula Duarte <74932085+apduartte@users.noreply.github.com>
2025-11-12 16:20:30 -0300

    Update providers.tf

--- nextcloud-aws-terraform/providers.tf
+++ nextcloud-aws-terraform/providers.tf
@@ -1,9 +1,16 @@
+## providers.tf
+```hcl
 terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
+required_providers {
+aws = {
+source = "hashicorp/aws"
+version = ">= 5.0"
 }
+}
+}
+
+
+provider "aws" {
+region = var.region
+}
+```
