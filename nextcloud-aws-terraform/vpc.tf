81e86a92d91009eed66a0821db322a8cdc6c8deb
Ana Paula Duarte <74932085+apduartte@users.noreply.github.com>
2025-11-12 16:32:21 -0300

    Create vpc.tf

---
+++ nextcloud-aws-terraform/vpc.tf
@@ -0,0 +1,19 @@
+## vpc.tf (módulo oficial VPC)
+```hcl
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 5.0"
+
+  name = "${var.project_name}-vpc"
+  cidr = var.vpc_cidr
+
+  azs             = var.azs
+  public_subnets  = var.public_cidrs
+  private_subnets = var.private_cidrs
+
+  enable_nat_gateway  = true
+  single_nat_gateway  = true
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+}
+```
