81db51f109d22192776fca5749a7db41f1905a33
Ana Paula Duarte <74932085+apduartte@users.noreply.github.com>
2025-11-12 16:34:21 -0300

    Update efs.tf

--- nextcloud-aws-terraform/efs.tf
+++ nextcloud-aws-terraform/efs.tf
@@ -1,15 +1,29 @@
-# efs.tf
-resource "aws_efs_file_system" "nextcloud" {
-  encrypted        = true
+## efs.tf
+```hcl
+resource "aws_efs_file_system" "this" {
+  creation_token  = "${var.project_name}-efs"
   performance_mode = "generalPurpose"
   throughput_mode  = "bursting"
-  tags             = merge(var.tags, { Name = "nextcloud-efs" })
+
+  lifecycle_policy {
+    transition_to_ia = "AFTER_30_DAYS"
+  }
+
+  tags = {
+    Name = "${var.project_name}-efs"
+  }
 }
 
-resource "aws_efs_mount_target" "nextcloud" {
-  for_each        = toset(module.vpc.private_subnets)
-  file_system_id  = aws_efs_file_system.nextcloud.id
-  subnet_id       = each.value
-  security_groups = [aws_security_group.efs_sg.id]  # ver item 2
+resource "aws_efs_mount_target" "a" {
+  file_system_id  = aws_efs_file_system.this.id
+  subnet_id       = module.vpc.private_subnets[0]
+  security_groups = [aws_security_group.efs.id]
 }
- 
+
+resource "aws_efs_mount_target" "b" {
+  file_system_id  = aws_efs_file_system.this.id
+  subnet_id       = module.vpc.private_subnets[1]
+  security_groups = [aws_security_group.efs.id]
+}
+```
+
