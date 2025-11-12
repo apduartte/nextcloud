f62c9d49d6c2781ab70ffae379678a20064e21f3
Ana Paula Duarte <74932085+apduartte@users.noreply.github.com>
2025-11-12 16:36:04 -0300

    Update alb.tf

--- nextcloud-aws-terraform/alb.tf
+++ nextcloud-aws-terraform/alb.tf
@@ -1,47 +1,66 @@
-
-resource "aws_lb" "nextcloud_alb" {
-  name               = "nextcloud-alb"
-  internal           = false
+## alb.tf
+```hcl
+resource "aws_lb" "this" {
+  name               = "${var.project_name}-alb"
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
+  security_groups    = [aws_security_group.alb.id]
   subnets            = module.vpc.public_subnets
-
-  enable_deletion_protection = false
-
-  tags = var.tags
 }
 
-resource "aws_lb_target_group" "nextcloud_tg" {
-  name_prefix = "nxt-"
-  port        = 80
-  protocol    = "HTTP"
-  target_type = "instance"
-  vpc_id      = module.vpc.vpc_id
+resource "aws_lb_target_group" "this" {
+  name     = "${var.project_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = module.vpc.vpc_id
 
   health_check {
-    path                = "/"
-    port                = "80"
-    healthy_threshold   = 3
-    unhealthy_threshold = 3
-    timeout             = 5
+    path                = "/status.php"
+    protocol            = "HTTP"
+    matcher             = "200-399"
+    healthy_threshold   = 2
+    unhealthy_threshold = 5
     interval            = 30
-    matcher             = "200"
+    timeout             = 5
   }
-
-  tags = var.tags
 }
 
-resource "aws_lb_listener" "nextcloud_listener" {
-  load_balancer_arn = aws_lb.nextcloud_alb.arn
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.this.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.nextcloud_tg.arn
+    type = var.enable_https ? "redirect" : "forward"
+
+    dynamic "redirect" {
+      for_each = var.enable_https ? [1] : []
+      content {
+        protocol   = "HTTPS"
+        port       = "443"
+        status_code = "HTTP_301"
+      }
+    }
+
+    dynamic "forward" {
+      for_each = var.enable_https ? [] : [1]
+      content {
+        target_group_arn = aws_lb_target_group.this.arn
+      }
+    }
   }
 }
 
-output "alb_dns_name" {
-  value = aws_lb.nextcloud_alb.dns_name
+resource "aws_lb_listener" "https" {
+  count             = var.enable_https ? 1 : 0
+  load_balancer_arn = aws_lb.this.arn
+  port              = 443
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS13-1-2-2021-06"
+  certificate_arn   = var.acm_certificate_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.this.arn
+  }
 }
+```
